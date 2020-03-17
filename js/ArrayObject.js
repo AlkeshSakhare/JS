659e3188de498818c537cd849a5792677cac08e9
unknown <akleshsakhare@yahoo.com>
2020-03-17 22:21:21 +0530

    arrays

--- js/ArrayObject.js
+++ js/ArrayObject.js
@@ -1,5 +1,5 @@
 /*
-manipulating primitive data in Array
+manipulating primitive data in Arrays
 */
 
 var arrayEx=[0,1,2,3,4,5,6,7,8,9];
@@ -30,7 +30,7 @@ console.log('remove 5 element from 5th position',arrayEx);
 
 
 /*
-finding refernce type in array
+finding refernce type in arrays
 */
 
 var arrayRef=[
